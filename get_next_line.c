27058acbe990cce23a9f2ded98e99b805ca5a7de
uygarpolat <uygarpolat@gmail.com>
2024-04-25 10:41:55 +0300

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,34 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 08:28:50 by upolat            #+#    #+#             */
-/*   Updated: 2024/04/25 08:30:00 by upolat           ###   ########.fr       */
+/*   Updated: 2024/04/25 10:37:13 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "get_next_line.h"
+
+char *get_next_line(int fd);
 {
-	
+	char	buffer[1];
+	char	*str;
+	size_t	i;
+	size_t	bytes_read;
+	size_t	len;
+
+	len = 0;
+	while (read(fd, buffer, 1) > 0 && *buffer != '\n')
+		len++;
+	str = malloc(sizeof(char) * (len + 1));
+	return (len);
 }
+
+#include <stdio.h>
 
 int main(void)
 {
-	
+	int	fd;
+
+	fd = open("test.txt", O_RDONLY);
+	printf("Characters until the first tab: %zu.\n", get_next_newline_char(fd));
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: upolat <upolat@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 08:30:20 by upolat            #+#    #+#             */
-/*   Updated: 2024/04/25 08:33:39 by upolat           ###   ########.fr       */
+/*   Updated: 2024/04/25 09:16:15 by upolat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-# define
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 char *get_next_line(int fd);
 
